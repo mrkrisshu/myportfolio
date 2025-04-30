781b6f6f55d7e0bc4fb2ea96ab9186aa9efa7dd1
Krishna Bantola D <67815735+mrkrisshu@users.noreply.github.com>
2025-04-30 23:31:42 +0530

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -94,7 +94,7 @@ const Contact = () => {
               name="email"
               type="email"
               className="field-input field-input-focus"
-              placeholder="JohnDoe@email.com"
+              placeholder="mrkrisshu@gmail.com"
               autoComplete="email"
               value={formData.email}
               onChange={handleChange}
